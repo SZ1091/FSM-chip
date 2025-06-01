30786731fe58e3fb11527fdff8103f7c69073811
SZ1091 <szamoral2@miumg.edu.gt>
2025-05-31 18:20:17 -0600

    Update Clock.v

--- src/Clock.v
+++ src/Clock.v
@@ -1,13 +1,16 @@
 `timescale 1ns / 1ps
 
-module Clock(input clk,
-             output reloj);
+module Clock (
+    input clk,
+    output wire reloj
+);
 
-reg [31:0] myreg;
+    reg [31:0] myreg = 0;
 
-always @(posedge clk)
-    myreg <= myreg + 1;
+    always @(posedge clk) begin
+        myreg <= myreg + 1;
+    end
 
-assign reloj = myreg[27];
+    assign reloj = myreg[27];
 
 endmodule
